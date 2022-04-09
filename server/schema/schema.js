bdd2b85a185490a84470d022af342bd5e76c6cf0
Ishaan Thakur <ishaant99@gmail.com>
2022-04-09 12:46:06 -0300

    Schema and Root Query

---
+++ server/schema/schema.js
@@ -0,0 +1,52 @@
+const graphql = require('graphql');
+
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// DUMMY DATA
+const books = [
+  {
+    name: 'Name of the wind',
+    genre: 'Fantasy',
+    id: 1,
+  },
+  {
+    name: 'The Final Empire',
+    genre: 'Fantasy',
+    id: 2,
+  },
+  {
+    name: 'The Long Earth',
+    genre: 'Sci-Fi',
+    id: 3,
+  },
+];
+
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  fields() {
+    return {
+      id: { type: GraphQLString },
+      name: { type: GraphQLString },
+      genre: { type: GraphQLString },
+    };
+  },
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        // code to get data from db
+        const findBook = books.find(cur => cur.id === args.id);
+        return findBook;
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
