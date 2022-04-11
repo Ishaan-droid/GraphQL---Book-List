156712f9222c74377673208d2ed3f34054c0cb5a
Ishaan Thakur <ishaant99@gmail.com>
2022-04-11 19:41:42 -0300

    Create React App

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import BookList from './components/BookList/BookList';
+
+function App() {
+  return (
+    <div id="main">
+      <h1>Ishaan's Reading List</h1>
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
