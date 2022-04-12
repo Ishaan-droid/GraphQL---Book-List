b1a3ddf7f9a40b9087c7d6b936cfb322fed04e3c
Ishaan Thakur <ishaant99@gmail.com>
2022-04-12 20:49:15 -0300

    Mutations with React

--- client/src/components/AddBook/AddBook.js
+++ client/src/components/AddBook/AddBook.js
@@ -1,7 +1,15 @@
-const { useQuery } = require('@apollo/client');
-const { getAuthorQuery } = require('../../Utils/Queries');
+import { useState } from 'react';
+
+const { useQuery, useMutation } = require('@apollo/client');
+const { getAuthorQuery, addBookMutation } = require('../../Utils/Queries');
 
 const AddBook = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    genre: '',
+    authorId: '',
+  });
+
   const { loading, data } = useQuery(getAuthorQuery);
 
   const displayAuthors = loading ? (
@@ -14,19 +22,56 @@ const AddBook = () => {
     ))
   );
 
+  const nameHandler = e =>
+    setFormData(prevValue => {
+      return {
+        ...prevValue,
+        name: e.target.value,
+      };
+    });
+
+  const genreHandler = e =>
+    setFormData(prevValue => {
+      return {
+        ...prevValue,
+        genre: e.target.value,
+      };
+    });
+
+  const authorHandler = e =>
+    setFormData(prevValue => {
+      return {
+        ...prevValue,
+        authorId: e.target.value,
+      };
+    });
+
+  const [createBook] = useMutation(addBookMutation, {
+    variables: {
+      name: formData.name,
+      genre: formData.genre,
+      authorId: formData.authorId,
+    },
+  });
+
+  const submitForm = e => {
+    e.preventDefault();
+    createBook();
+  };
+
   return (
-    <form id="addBook">
+    <form id="addBook" onSubmit={submitForm}>
       <div className="field">
         <label>Book Name:</label>
-        <input type="text" />
+        <input type="text" onChange={nameHandler} />
       </div>
       <div className="field">
         <label>Genre:</label>
-        <input type="text" />
+        <input type="text" onChange={genreHandler} />
       </div>
       <div className="field">
         <label>Author:</label>
-        <select>
+        <select onChange={authorHandler}>
           <option>Select Author</option>
           {displayAuthors}
         </select>
